d9f2690c466dfd6e40e75a070fb8e4b467df2841
1696699 <120693972+1696699@users.noreply.github.com>
2024-03-27 20:42:45 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function setup(){
+    canvas = createCanvas(500, 425);
+    canvas.position(600, 150);
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+}
+function draw(){
+    background("lightBlue");
+}
